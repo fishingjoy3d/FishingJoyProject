fdc05eacae8acc407c3a9b7152cdda7b428aef8e
fishingjoy3d <fishingjoy3d@163.com>
2017-05-02 11:48:47 +0800

    androidnative

---
+++ BuyuSources/NewClient/Assets/Scripts/AndroidNative/AN_PoupsProxy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AN_PoupsProxy{
+
+    private const string CLASS_NAME = "com.androidnative.popups.PopUpsManager";
+    private static AndroidJavaClass androidNativeClass;
+
+    public static void CallActivityFunction(string methodName, params object[] args)
+    {
+#if UNITY_ANDROID
+        if (Application.platform != RuntimePlatform.Android)
+        {
+            return;
+        }
+
+        try
+        {
+            if (androidNativeClass == null)
+            {
+                androidNativeClass = new AndroidJavaClass(CLASS_NAME);
+            }
+            AndroidJavaClass jc = new AndroidJavaClass(CLASS_NAME);
+            androidNativeClass.CallStatic(methodName, args);
+        }
+        catch (System.Exception ex)
+        {
+            Debug.LogWarning(ex.Message);
+        }
+#endif
+    }
+
+    public static void ShowToast(string str)
+    {
+        CallActivityFunction("showToast", str);
+    }
+}
